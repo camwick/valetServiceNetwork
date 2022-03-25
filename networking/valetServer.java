d1ed0108dc3687a8b42b592273a929ed61d5704a
camwick <cwickers@pnw.edu>
2022-03-25 16:52:52 -0500

    add, select, remove functions in place

--- networking/valetServer.java
+++ networking/valetServer.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class valetServer {
   private int floorCount;
@@ -15,6 +16,43 @@ public class valetServer {
   public valetServer(int floorCount, int maxCarPerFloor) {
     this.floorCount = floorCount;
     this.maxCarPerFloor = maxCarPerFloor;
+  }
+
+  private static void select(Connection conn, String firstName, String lastName) {
+    // select floor and car
+    try {
+      // prepare query
+      PreparedStatement query = conn
+          .prepareStatement("SELECT floor, color, make, model FROM Garage WHERE firstName = ? AND lastName = ?");
+      query.setString(1, firstName);
+      query.setString(2, lastName);
+
+      // execute query
+      ResultSet result = query.executeQuery();
+
+      System.out.println("Floor: " + result.getInt("floor"));
+      System.out.println(
+          "Car: " + result.getString("color") + " " + result.getString("make") + " " + result.getString("model"));
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void remove(Connection conn, String firstName, String lastName) {
+    try {
+      // prepare query
+      PreparedStatement query = conn
+          .prepareStatement("DELETE FROM Garage WHERE firstName = ? AND lastName = ?");
+      query.setString(1, firstName);
+      query.setString(2, lastName);
+
+      // execute query
+      query.executeUpdate();
+
+      System.out.println("Car removed from database");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) {
@@ -41,6 +79,7 @@ public class valetServer {
     Scanner input = new Scanner(System.in);
 
     String response = "";
+    String[] names;
     while (!response.equals("done")) {
       response = input.nextLine().trim();
 
@@ -66,10 +105,52 @@ public class valetServer {
           } catch (SQLException e) {
             e.printStackTrace();
           }
+
+          break;
+        case "select":
+          // get name
+          System.out.print("First and last name: ");
+          response = input.nextLine().trim();
+
+          names = response.split(" ");
+
+          select(conn, names[0], names[1]);
+
+          break;
+        case "remove":
+          // get name
+          System.out.print("First and last name: ");
+          response = input.nextLine().trim();
+
+          names = response.split(" ");
+
+          // delete from database
+          remove(conn, names[0], names[1]);
+
+          break;
+        case "select and remove":
+          // get name
+          System.out.print("First and last name: ");
+          response = input.nextLine().trim();
+
+          names = response.split(" ");
+
+          // select floor and car
+          select(conn, names[0], names[1]);
+
+          // delete from database
+          remove(conn, names[0], names[1]);
+
           break;
       }
     }
 
     input.close();
+
+    try {
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }

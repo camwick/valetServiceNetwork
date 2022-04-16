a0a3d979bdfd8696e39e71969f6cb7df38c76c6f
camwick <cwickers@pnw.edu>
2022-04-15 19:33:18 -0500

    added the 3 methods from valetImpl.java

--- src/valetInterface.java
+++ src/valetInterface.java
@@ -1,3 +1,12 @@
-public class valetInterface {
+import java.rmi.Remote;
 
+public interface valetInterface extends Remote {
+  public int getPort() throws java.rmi.RemoteException;
+
+  public String addCar(int floor, String firstName, String lastName, String color, String make, String model)
+      throws java.rmi.RemoteException;
+
+  public String select(String firstName, String lastName) throws java.rmi.RemoteException;
+
+  public String remove(String firstName, String lastName) throws java.rmi.RemoteException;
 }

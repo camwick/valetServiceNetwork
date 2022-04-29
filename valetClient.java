72890b42ecd230d8ed7ccdecca864569b62b9a75
camwick <cwickers@pnw.edu>
2022-04-29 15:22:40 -0500

    started implementing client interactions

--- valetClient.java
+++ valetClient.java
@@ -3,31 +3,68 @@ import java.io.InputStreamReader;
 import java.rmi.Naming;
 
 public class valetClient {
-    public static void main(String[] args) {
-        try {
-            int RMIPort;
-            String hostName;
-            String portNum;
-            InputStreamReader is = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(is);
+  public static void main(String[] args) {
+    try {
+      int RMIPort;
+      String hostName;
+      String portNum;
+      InputStreamReader is = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader(is);
 
-            // obtaining RMI stuff
-            System.out.println("Enter the RMI Registry Host Name:");
-            hostName = br.readLine();
-            System.out.println("Enter the RMI Registry Port Number:");
-            portNum = br.readLine();
-            RMIPort = Integer.parseInt(portNum);
+      // obtaining RMI stuff
+      System.out.println("Enter the RMI Registry Host Name:");
+      hostName = br.readLine();
+      System.out.println("Enter the RMI Registry Port Number:");
+      portNum = br.readLine();
+      RMIPort = Integer.parseInt(portNum);
 
-            // grab remote object
-            String registryURL = "rmi://" + hostName + ":" + portNum + "/valet";
-            valetInterface h = (valetInterface) Naming.lookup(registryURL);
+      // grab remote object
+      String registryURL = "rmi://" + hostName + ":" + portNum + "/valet";
+      valetInterface valet = (valetInterface) Naming.lookup(registryURL);
 
-            // invoke methods here...
-            System.out.println(h.test());
+      // invoke methods here...
+      boolean loop = true;
+      while (loop) {
+        System.out.println("\nWhat would you to do?");
+        System.out.println("1) Add a car to the DB\n2) View entry\n3) Remove a car from the DB\n4) Quit");
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        String response = br.readLine().trim();
+        String[] queryInfo;
+        switch (Integer.parseInt(response)) {
+          case 1:
+            System.out.println("Supply:\n- Floor Number\n- First Name\n- Last Name\n- Color of Car\n- Make & Model");
+
+            response = br.readLine().trim();
+            queryInfo = response.split(" ");
+            response = valet.addCar(Integer.parseInt(queryInfo[0]), queryInfo[1], queryInfo[2], queryInfo[3],
+                queryInfo[4],
+                queryInfo[5]);
+            System.out.println(response.trim());
+            break;
+          case 2:
+            System.out.print("Enter first and last name: ");
+            response = br.readLine().trim();
+            queryInfo = response.split(" ");
+            response = valet.select(queryInfo[0], queryInfo[1]);
+            System.out.println(response);
+            break;
+          case 3:
+            System.out.print("Enter first and last name: ");
+            response = br.readLine().trim();
+            queryInfo = response.split(" ");
+            response = valet.remove(queryInfo[0], queryInfo[1]);
+            System.out.println(response);
+            break;
+          case 4:
+            valet.quit();
+            loop = false;
+            break;
         }
+      }
 
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
+  }
 }
--- valetImpl.java
+++ valetImpl.java
@@ -7,116 +7,132 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class valetImpl extends UnicastRemoteObject implements valetInterface {
-    private int floorCount;
-    private int maxCarPerFloor;
-    private Connection conn = null;
+  private int floorCount;
+  private int maxCarPerFloor;
+  private Connection conn = null;
 
-    public valetImpl() throws RemoteException, SQLException {
-        this(4, 50);
+  public valetImpl() throws RemoteException, SQLException {
+    this(4, 50);
+  }
+
+  public valetImpl(int floorCount, int maxCarPerFloor) throws RemoteException, SQLException {
+    super();
+    this.floorCount = floorCount;
+    this.maxCarPerFloor = maxCarPerFloor;
+    this.conn = DriverManager.getConnection("jdbc:sqlite:./valetDB.db");
+  }
+
+  @Override
+  public String addCar(int floor, String firstName, String lastName, String color, String make, String model)
+      throws RemoteException {
+    String output = "";
+
+    if (floor > this.floorCount)
+      return "Invalid floor number";
+
+    try {
+      PreparedStatement query = this.conn.prepareStatement("SELECT Count(floor) FROM Garage WHERE floor = ?");
+      query.setInt(1, floor);
+      ResultSet result = query.executeQuery();
+
+      if (result.getInt("Count(floor)") >= this.maxCarPerFloor)
+        return "Floor is full";
+
+      // create query
+      query = this.conn.prepareStatement(
+          "INSERT INTO Garage (floor, firstName, lastName, color, make, model) VALUES (?, ?, ?, ?, ?, ?)");
+      query.setInt(1, floor);
+      query.setString(2, firstName);
+      query.setString(3, lastName);
+      query.setString(4, color);
+      query.setString(5, make);
+      query.setString(6, model);
+
+      // run query
+      query.executeUpdate();
+      query.close();
+
+      output += "Successfully added car";
+      System.out.println("Added " + firstName + " " + lastName + "'s car to database.");
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    public valetImpl(int floorCount, int maxCarPerFloor) throws RemoteException, SQLException {
-        super();
-        this.floorCount = floorCount;
-        this.maxCarPerFloor = maxCarPerFloor;
-        this.conn = DriverManager.getConnection("jdbc:sqlite:./valetDB.db");
+    return output;
+  }
+
+  @Override
+  public String select(String firstName, String lastName) throws RemoteException {
+    String output = "";
+
+    // select floor and car
+    try {
+      // prepare query
+      PreparedStatement query = this.conn
+          .prepareStatement(
+              "SELECT floor, color, make, model FROM Garage WHERE firstName = ? AND lastName = ?");
+      query.setString(1, firstName);
+      query.setString(2, lastName);
+
+      // execute query
+      ResultSet result = query.executeQuery();
+      while (result.next()) {
+        if (!result.isClosed()) {
+          output += "Floor: " + result.getInt("floor") + "\n";
+          output += "Car: " + result.getString("color") + " " + result.getString("make") + " "
+              + result.getString("model") + "\n";
+        } else
+          output += "Name is not in database";
+      }
+
+      result.close();
+      query.close();
+    } catch (SQLException e) {
+      System.out.println("test");
+      e.printStackTrace();
     }
 
-    @Override
-    public String addCar(int floor, String firstName, String lastName, String color, String make, String model)
-            throws RemoteException {
-        String output = "";
+    return output;
+  }
 
-        try {
-            // create query
-            PreparedStatement query = this.conn.prepareStatement(
-                    "INSERT INTO Garage (floor, firstName, lastName, color, make, model) VALUES (?, ?, ?, ?, ?, ?)");
-            query.setInt(1, floor);
-            query.setString(2, firstName);
-            query.setString(3, lastName);
-            query.setString(4, color);
-            query.setString(5, make);
-            query.setString(6, model);
+  @Override
+  public String remove(String firstName, String lastName) throws RemoteException {
+    String output = "";
 
-            // run query
-            query.executeUpdate();
-            query.close();
+    try {
+      // prepare query
+      PreparedStatement query = this.conn
+          .prepareStatement("DELETE FROM Garage WHERE firstName = ? AND lastName = ?");
+      query.setString(1, firstName);
+      query.setString(2, lastName);
 
-            output += "Successfully added car";
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+      // execute query
+      query.executeUpdate();
+      query.close();
 
-        return output;
+      output += "Car removed from database";
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    @Override
-    public String select(String firstName, String lastName) throws RemoteException {
-        String output = "";
+    return output;
+  }
 
-        // select floor and car
-        try {
-            // prepare query
-            PreparedStatement query = this.conn
-                    .prepareStatement(
-                            "SELECT floor, color, make, model FROM Garage WHERE firstName = ? AND lastName = ?");
-            query.setString(1, firstName);
-            query.setString(2, lastName);
+  @Override
+  public void quit() throws RemoteException, SQLException {
+    this.conn.close();
+  }
 
-            // execute query
-            ResultSet result = query.executeQuery();
-            query.close();
+  @Override
+  public String test() throws RemoteException {
+    return "this is a test";
+  }
 
-            output += "Floor: " + result.getInt("floor");
-            output += "Car: " + result.getString("color") + " " + result.getString("make") + " "
-                    + result.getString("model");
-            result.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return output;
-    }
-
-    @Override
-    public String remove(String firstName, String lastName) throws RemoteException {
-        String output = "";
-
-        try {
-            // prepare query
-            PreparedStatement query = this.conn
-                    .prepareStatement("DELETE FROM Garage WHERE firstName = ? AND lastName = ?");
-            query.setString(1, firstName);
-            query.setString(2, lastName);
-
-            // execute query
-            query.executeUpdate();
-            query.close();
-
-            output += "Car removed from database";
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return output;
-    }
-
-    @Override
-    public void quit() throws RemoteException, SQLException {
-        this.conn.close();
-        System.exit(1);
-    }
-
-    @Override
-    public String test() throws RemoteException {
-        return "this is a test";
-    }
-
-    public static void main(String[] args) throws RemoteException, SQLException {
-        valetImpl test = new valetImpl();
-        System.out.println(test.addCar(2, "Cameron", "Wickersham", "white", "Honda", "Civic"));
-        test.quit();
-        System.out.println("test");
-    }
+  public static void main(String[] args) throws RemoteException, SQLException {
+    valetImpl test = new valetImpl();
+    System.out.println(test.addCar(2, "Cameron", "Wickersham", "white", "Honda", "Civic"));
+    test.quit();
+    System.out.println("test");
+  }
 
 }
